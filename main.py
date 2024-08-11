11b9f6ae40b8b7b615442f6ff6c7de7f45cffa7c
kestrel41x4 <kestrel41x4@gmail.com>
2024-08-10 23:44:25 -0500

    Initialized Project

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+
+from sklearn.model_selection import train_test_split
+
+from keras.preprocessing.image import ImageDataGenerator
